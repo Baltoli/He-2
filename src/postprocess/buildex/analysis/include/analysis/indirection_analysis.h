a6fdb0f074fb75e3b2dff7fed775ac4daed7b713
Bruce Collie <brucecollie82@gmail.com>
2020-09-30 21:39:58 +0100

    Build analysis properly

--- src/postprocess/buildex/analysis/include/analysis/indirection_analysis.h
+++ src/postprocess/buildex/analysis/include/analysis/indirection_analysis.h
@@ -1,17 +1,18 @@
 #ifndef BUILDEX_INDIRECTION
 #define BUILDEX_INDIRECTION
 
-#include <vector>
+#include <analysis/staticinfo.h>
+#include <analysis/x86_analysis.h>
+
+#include <memory/memregions.h>
+
 #include <stdint.h>
+#include <vector>
 
-#include "memory/memregions.h"
-#include "analysis/staticinfo.h"
-#include "analysis/x86_analysis.h"
-
-std::vector< std::vector<uint32_t> > find_dependant_statements_with_indirection
-					(vec_cinstr &instrs, std::vector<mem_regions_t *> mem, std::vector<Static_Info *> static_info, std::vector<uint32_t> start_points);
-void populate_dependant_indireciton(std::vector<Static_Info *> &static_info, std::vector<uint32_t> dep);
-
-
+std::vector<std::vector<uint32_t>> find_dependant_statements_with_indirection(
+    vec_cinstr& instrs, std::vector<mem_regions_t*> mem,
+    std::vector<Static_Info*> static_info, std::vector<uint32_t> start_points);
+void populate_dependant_indireciton(
+    std::vector<Static_Info*>& static_info, std::vector<uint32_t> dep);
 
 #endif
--- src/postprocess/buildex/analysis/include/analysis/preprocess.h
+++ src/postprocess/buildex/analysis/include/analysis/preprocess.h
@@ -1,28 +1,33 @@
 #ifndef _PREPROCESS_H
 #define _PREPROCESS_H
 
-#include  "..\..\..\dr_clients\include\output.h"
-#include <string>
+#include <analysis/staticinfo.h>
+#include <analysis/x86_analysis.h>
+
+#include <common/output.h>
+
 #include <iostream>
 #include <stdint.h>
+#include <string>
 #include <vector>
 
-#include "analysis/x86_analysis.h"
-#include "analysis/staticinfo.h"
-
 /* preprocess mutating routines */
-void filter_disasm_vector(vec_cinstr &instrs, std::vector<Static_Info *> &static_info);
-					
+void filter_disasm_vector(
+    vec_cinstr& instrs, std::vector<Static_Info*>& static_info);
+
 /* information gathtering routines */
-std::vector<uint32_t> get_instrace_startpoints(vec_cinstr &instrs, uint32_t pc);
-std::vector<uint32_t> get_instrace_startpoints(vec_cinstr &instrs, std::vector<uint32_t> pcs);
+std::vector<uint32_t> get_instrace_startpoints(vec_cinstr& instrs, uint32_t pc);
+std::vector<uint32_t>
+get_instrace_startpoints(vec_cinstr& instrs, std::vector<uint32_t> pcs);
 
-vec_cinstr filter_instr_trace(uint32_t start_pc, uint32_t end_pc, vec_cinstr &unfiltered_instrs);
-vec_cinstr filter_instr_trace(std::vector<uint32_t> start_pcs, std::vector<uint32_t> end_pcs, vec_cinstr &unfiltered_instrs);
+vec_cinstr filter_instr_trace(
+    uint32_t start_pc, uint32_t end_pc, vec_cinstr& unfiltered_instrs);
+vec_cinstr filter_instr_trace(
+    std::vector<uint32_t> start_pcs, std::vector<uint32_t> end_pcs,
+    vec_cinstr& unfiltered_instrs);
 
-std::pair<uint32_t, uint32_t> get_start_end_pcs(std::vector<Static_Info *> &infos, Static_Info * first);
+std::pair<uint32_t, uint32_t>
+get_start_end_pcs(std::vector<Static_Info*>& infos, Static_Info* first);
 
+#endif
 
-
- #endif
- 
