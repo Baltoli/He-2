3a3f2d1b874d9d68f0f525cea33eb6ea2790be61
Bruce Collie <brucecollie82@gmail.com>
2020-09-30 21:46:40 +0100

    More fixes

--- src/postprocess/buildex/analysis/src/preprocess.cpp
+++ src/postprocess/buildex/analysis/src/preprocess.cpp
@@ -1,177 +1,168 @@
+#include <analysis/preprocess.h>
+#include <analysis/staticinfo.h>
+#include <analysis/x86_analysis.h>
+
+#include <common/common_defines.h>
+#include <common/meminfo.h>
+
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
 #include <string>
-
-#include "analysis/preprocess.h"
-#include "analysis/x86_analysis.h"
-#include "analysis/staticinfo.h"
-
-#include "meminfo.h"
-#include "common_defines.h"
-
 
 using namespace std;
 
-void filter_disasm_vector(vec_cinstr &instrs, vector<Static_Info *> &static_info){
+void filter_disasm_vector(vec_cinstr& instrs, vector<Static_Info*>& static_info)
+{
 
-	for (int i = 0; i < static_info.size(); i++){
+  for (int i = 0; i < static_info.size(); i++) {
 
-		uint32_t pc = static_info[i]->pc;
+    uint32_t pc = static_info[i]->pc;
 
-		bool found = false;
-		for (int j = 0; j < instrs.size(); j++){
-			cinstr_t * instr = instrs[j].first;
-			if (instr->pc == pc){
-				found = true;
-				break;
-			}
-		}
+    bool found = false;
+    for (int j = 0; j < instrs.size(); j++) {
+      cinstr_t* instr = instrs[j].first;
+      if (instr->pc == pc) {
+        found = true;
+        break;
+      }
+    }
 
-		if (!found){
-			static_info.erase(static_info.begin() + i--);
-		}
-
-	}
-
+    if (!found) {
+      static_info.erase(static_info.begin() + i--);
+    }
+  }
 }
 
-std::vector<uint32_t> get_instrace_startpoints(vec_cinstr &instrs, uint32_t pc){
+std::vector<uint32_t> get_instrace_startpoints(vec_cinstr& instrs, uint32_t pc)
+{
 
-	vector<uint32_t> start_points;
-	int line = 0;
+  vector<uint32_t> start_points;
+  int line = 0;
 
-	for (int i = 0; i < instrs.size(); i++){
-		cinstr_t * instr = instrs[i].first;
-		if (instr != NULL){
-			if (instr->pc == pc){
-				start_points.push_back(i + 1);
-			}
-		}
-	}
+  for (int i = 0; i < instrs.size(); i++) {
+    cinstr_t* instr = instrs[i].first;
+    if (instr != NULL) {
+      if (instr->pc == pc) {
+        start_points.push_back(i + 1);
+      }
+    }
+  }
 
-	return start_points;
-
+  return start_points;
 }
 
-std::vector<uint32_t> get_instrace_startpoints(vec_cinstr &instrs, vector<uint32_t> pcs){
+std::vector<uint32_t>
+get_instrace_startpoints(vec_cinstr& instrs, vector<uint32_t> pcs)
+{
 
-	vector<uint32_t> start_points;
-	int line = 0;
+  vector<uint32_t> start_points;
+  int line = 0;
 
-	for (int i = 0; i < instrs.size(); i++){
-		cinstr_t * instr = instrs[i].first;
-		if (instr != NULL){
-			for (int j = 0; j < pcs.size(); j++){
-				if (pcs[j] == instr->pc){
-					start_points.push_back(i + 1);
-				}
-			}
-		}
-	}
+  for (int i = 0; i < instrs.size(); i++) {
+    cinstr_t* instr = instrs[i].first;
+    if (instr != NULL) {
+      for (int j = 0; j < pcs.size(); j++) {
+        if (pcs[j] == instr->pc) {
+          start_points.push_back(i + 1);
+        }
+      }
+    }
+  }
 
-	return start_points;
-
+  return start_points;
 }
 
+vec_cinstr filter_instr_trace(
+    uint32_t start_pc, uint32_t end_pc, vec_cinstr& unfiltered_instrs)
+{
 
-vec_cinstr filter_instr_trace(uint32_t start_pc, uint32_t end_pc, vec_cinstr &unfiltered_instrs){
+  vec_cinstr instrs;
+  bool start = false;
 
+  for (int i = 0; i < unfiltered_instrs.size(); i++) {
+    if (unfiltered_instrs[i].first->pc == start_pc) {
+      start = true;
+    }
+    if (start)
+      instrs.push_back(unfiltered_instrs[i]);
+    if (unfiltered_instrs[i].first->pc == end_pc) {
+      start = false;
+    }
+  }
 
-	vec_cinstr instrs;
-	bool start = false;
-
-	for (int i = 0; i < unfiltered_instrs.size(); i++){
-		if (unfiltered_instrs[i].first->pc == start_pc){
-			start = true;
-		}
-		if (start) instrs.push_back(unfiltered_instrs[i]);
-		if (unfiltered_instrs[i].first->pc == end_pc){
-			start = false;
-		}
-	}
-
-	return instrs;
-
-
+  return instrs;
 }
 
+int check_pc(vector<uint32_t> candidates, uint32_t pc)
+{
 
-int check_pc(vector<uint32_t> candidates, uint32_t pc){
+  for (int i = 0; i < candidates.size(); i++) {
+    if (candidates[i] == pc)
+      return i;
+  }
 
-	for (int i = 0; i < candidates.size(); i++){
-		if (candidates[i] == pc) return i;
-	}
-
-
-	return -1;
-
-
+  return -1;
 }
 
-vec_cinstr filter_instr_trace(vector<uint32_t> start_pc, vector<uint32_t> end_pc, vec_cinstr &unfiltered_instrs){
+vec_cinstr filter_instr_trace(
+    vector<uint32_t> start_pc, vector<uint32_t> end_pc,
+    vec_cinstr& unfiltered_instrs)
+{
 
+  vec_cinstr instrs;
+  bool start = false;
+  int32_t index = -1;
 
-	vec_cinstr instrs;
-	bool start = false;
-	int32_t index = -1;
+  DEBUG_PRINT(("filtering the instruction trace by start,end pcs\n"), 2);
 
-	DEBUG_PRINT(("filtering the instruction trace by start,end pcs\n"), 2);
+  for (int i = 0; i < unfiltered_instrs.size(); i++) {
 
-	for (int i = 0; i < unfiltered_instrs.size(); i++){
+    if (!start) {
+      index = check_pc(start_pc, unfiltered_instrs[i].first->pc);
+      if (index != -1)
+        start = true;
+    }
 
-		if (!start){
-			index = check_pc(start_pc, unfiltered_instrs[i].first->pc);
-			if (index != -1) start = true;
-		}
+    if (start) {
+      instrs.push_back(unfiltered_instrs[i]);
+      unfiltered_instrs[i].second->example_line
+          = instrs.size() - 1; // check this??
+    }
 
-		if (start){
-			instrs.push_back(unfiltered_instrs[i]);
-			unfiltered_instrs[i].second->example_line = instrs.size() - 1; //check this??
-		}
+    if (start && unfiltered_instrs[i].first->pc == end_pc[index]) {
+      start = false;
+    }
+  }
 
-		if (start && unfiltered_instrs[i].first->pc == end_pc[index]){
-			start = false;
-		}
-	}
-
-	return instrs;
-
-
+  return instrs;
 }
-
 
 /* for a single function get start and end points */
-pair<uint32_t, uint32_t> get_start_end_pcs(vector<Static_Info *> &infos, Static_Info * first){
+pair<uint32_t, uint32_t>
+get_start_end_pcs(vector<Static_Info*>& infos, Static_Info* first)
+{
 
-	uint32_t start = first->pc;
+  uint32_t start = first->pc;
 
-	vector<uint32_t> pcs;
-	//get all ret instructions
-	for (int i = 0; i < infos.size(); i++){
-		if (infos[i]->disassembly.find("ret") != string::npos){
-			pcs.push_back(infos[i]->pc);
-		}
-	}
+  vector<uint32_t> pcs;
+  // get all ret instructions
+  for (int i = 0; i < infos.size(); i++) {
+    if (infos[i]->disassembly.find("ret") != string::npos) {
+      pcs.push_back(infos[i]->pc);
+    }
+  }
 
+  ASSERT_MSG(pcs.size() > 0, ("ERROR: no return instructions found\n"));
 
-	ASSERT_MSG(pcs.size() > 0, ("ERROR: no return instructions found\n"));
+  uint32_t end = 0;
+  for (int i = 0; i < pcs.size(); i++) {
+    if (pcs[i] > start) {
+      end = pcs[i];
+      break;
+    }
+  }
 
-	uint32_t end = 0;
-	for (int i = 0; i < pcs.size(); i++){
-		if (pcs[i] > start){
-			end = pcs[i]; break;
-		}
-	}
-
-	return make_pair(start, end);
-	
-
+  return make_pair(start, end);
 }
-
-
-
-
-
-
 
--- src/postprocess/buildex/trees/include/trees/trees.h
+++ src/postprocess/buildex/trees/include/trees/trees.h
@@ -1,229 +1,234 @@
- #ifndef _TREES_H
- #define _TREES_H
- 
- #include <stdint.h>
- #include "trees\nodes.h"
- #include "analysis\x86_analysis.h"
- 
- /* trees and their routines */
-
-typedef void *  (*node_mutator) (Node * node, void * value);
-typedef void *  (*return_mutator) (void * node_value, std::vector<void *> traverse_value, void * value);
-typedef Node *  (*node_to_node)(void * head, void * node, void * peripheral_data);
-
-void * empty_ret_mutator(void * value, vector<void *> values, void * ori_value);
-
- class Tree{
-
- protected:
-	 Node * head;
-	
-	 /* internal simplification routines */
-	 /* fill as they are written */
-
- private:
-
-	 void print_tree_recursive(Node * node, std::ostream &file);
-	 void collect_all_nodes(Node * node, std::vector<Node *> &nodes);
-	 static void copy_unrolled_tree_structure(Node * head,Node * from, Node * to, void * peripheral_data, node_to_node node_creation);
-	 static bool are_trees_similar(std::vector<Node *> node);
- protected:
-	 void * traverse_tree(Node * node, void * value, node_mutator mutator, return_mutator ret_mutator);
-	 void copy_exact_tree_structure(Tree * tree, void * peripheral_data, node_to_node node_creation);
-	 void copy_unrolled_tree_structure(Tree * tree, void * peripheral_data, node_to_node node_creation);
-
-
- public:
-
-	 uint32_t num_nodes;
-	 int32_t tree_num; /* this is for numbering the tree (most porabably based on output location - used in tree clustering) */
-	 static uint32_t num_paras;
-	 bool recursive;
-	 bool dummy_tree;
-
-
-	 Tree();
-	 ~Tree();
-	 
-	 Node * get_head();
-	 void set_head(Node * node);
-
-
-	 /* for tree simplification -> each type of tree will have its own serialization routines */
-	 virtual std::string serialize_tree() = 0;
-	 virtual void construct_tree(std::string stree) = 0;
-
-	 /* tree manipulation routines */
-	 void canonicalize_tree();
-	 void simplify_tree();
-	 void change_head_node();
-
-	 /* for printing purposes */
-	 void number_tree_nodes();
-	 void print_tree(std::ostream &file);
-	 void print_dot(std::ostream &file, std::string name, uint32_t number);
-
-	 bool are_trees_similar(Tree * tree);
-	 static bool are_trees_similar(std::vector<Tree *> trees);
-	 bool is_recursive(Node * node, std::vector<mem_regions_t *> &regions);
-	 
-	 void cleanup_visit();
-
-
-	 /* some tree transformations */
-	 void remove_assign_nodes();
-	 std::vector<mem_regions_t *> identify_intermediate_buffers(std::vector<mem_regions_t *> mem);
-	 void remove_multiplication();
-	 void simplify_immediates();
-	 void remove_minus_nodes();
-	 void remove_redundant_nodes();
-	 void convert_sub_nodes();
-	 void simplify_minus();
-	 void verify_minus();
-	 void remove_po_nodes();
-	 void remove_or_minus_1();
-	 void mark_recursive();
-	 void remove_identities();
- };
-
-
-
-
- /* Conc_Tree - expression tree from the instruction trace */
- 
- class Conc_Tree : public Tree {
-
- private:
-
-	 /* data structure for keeping the hash table bins */
-	 struct frontier_t {   
-		 Node ** bucket;
-		 int amount;
-	 };
-
-	 frontier_t * frontier;  /*this is actually a hash table keeping pointers to the Nodes already allocated */
-	 std::vector<uint32_t> mem_in_frontier; /* memoization structures for partial mem and reg writes and reads */
-	 
-	 bool func_inside;
-	 uint32_t func_index;
-
-	 
-	 Node * create_or_get_node(operand_t * opnd);
-	 void remove_from_frontier(operand_t * opnd);
-
-
-	 void get_full_overlap_nodes(std::vector<Node *> &nodes, operand_t * opnd);
-	 void split_partial_overlaps(std::vector < std::pair <Node *, std::vector<Node *> > > &nodes, operand_t * opnd, uint32_t hash);
-	 void get_partial_overlap_nodes(std::vector< std::pair<Node *, std::vector<Node *> > > &nodes, operand_t * opnd);
-
-	 void number_parameters(Node * node, vector<mem_regions_t *> regions);
-
-
- public:
-	
-	 /* this governs which conditionals affect the tree -> needed for predicate tree generation */
-	struct conditional_t {
-
-		 Jump_Info * jump_info;
-		 uint32_t line_cond; // this is the cond_pc location 
-		 uint32_t line_jump;
-		 Conc_Tree * tree; // we need to have two expressions for a single conditional
-		 bool taken;
-
-	 };
-
-	 std::vector<conditional_t *> conditionals;
-
-	 Conc_Tree();
-	 ~Conc_Tree();
-
-
-	 Node * search_node(operand_t * opnd);
-	 int generate_hash(operand_t * opnd);
-	 void add_to_frontier(int hash, Node * node);
-	 void remove_registers_from_frontier();
-
-	 //bool update_depandancy_backward(rinstr_t * instr, cinstr_t * cinstr, Static_Info * info, uint32_t line);
-	 bool update_depandancy_backward(rinstr_t * instr, cinstr_t * cinstr, Static_Info * info, uint32_t line, vector<mem_regions_t *> region, vector<Func_Info_t *> func_info);
-	 bool update_dependancy_forward(rinstr_t * instr, uint32_t pc, std::string disasm, uint32_t line);
-	 bool update_dependancy_forward_with_indirection(rinstr_t * instr, uint32_t pc, std::string disasm, uint32_t line);
-	 bool update_depandancy_forward_with_src(rinstr_t * instr, uint32_t pc, std::string disasm, uint32_t line, bool * src_dep);
-
-	 void add_address_dependancy(Node * node, operand_t * opnds);
-	 void process_forward_destination(operand_t * opnd);
-	 void remove_dest_forward(operand_t * opnd);
-
-	 void number_parameters(std::vector<mem_regions_t *> regions);
-	 std::string serialize_tree();
-	 void construct_tree(std::string stree);
-	 void print_conditionals();
-	 bool tree_add_to_frontier(rinstr_t * instr, Node * src);
- };
-
-
-
- /* Abs tree */
- class Abs_Tree : public Tree {
- 
- public:
-
-	 Abs_Tree();
-	 ~Abs_Tree();
-
-
-
-	 std::vector< std::pair<Abs_Tree *, bool > > conditional_trees;
-
-	 void build_abs_tree_exact(Conc_Tree * tree, std::vector<mem_regions_t *> &mem_regions);
-	 void build_abs_tree_unrolled(Conc_Tree * tree, std::vector<mem_regions_t *> &mem_regions);
-	 void seperate_intermediate_buffers();
-
-	 bool is_tree_recursive();
-	 Abs_Node * find_indirect_node(Abs_Node * node);
-	 
-	 std::vector<Abs_Node *> collect_input_nodes();
-
-	 std::string serialize_tree();
-	 void construct_tree(std::string stree);
-
-	 void tag_parameters();
-	 vector<Abs_Node *> retrieve_input_nodes();
-	 vector<Abs_Node *> retrieve_parameters();
-
-	 vector<Abs_Node *> get_buffer_region_nodes();
-
-	 uint32_t get_maximum_dimensions();
-
-	 void print_dot_algebraic(std::ostream &file, std::string name, uint32_t number, std::vector<std::string> vars);
-	 
-
- };
-
-
- /* compound abs tree */
- class Comp_Abs_Tree : public Tree {
-
- private:
-	 static void build_compound_tree_unrolled(Comp_Abs_Node *comp_node, std::vector<Abs_Node *> abs_nodes);
-	 void traverse_trees(std::vector<Abs_Node *> abs_nodes, 
-		 std::vector< std::pair < std::vector<Abs_Node *>, std::vector<int> > > &node_pos);
-
- public:
-
-	 Comp_Abs_Tree();
-	 ~Comp_Abs_Tree();
-	 
-	 void build_compound_tree_exact(std::vector<Abs_Tree *> abs_trees);
-	 void build_compound_tree_unrolled(std::vector<Abs_Tree *> abs_trees);
-	 void abstract_buffer_indexes();
-	 Abs_Tree * compound_to_abs_tree();
-
-	 std::string serialize_tree();
-	 void construct_tree(std::string stree);
- };
-
-
-
-
+#ifndef _TREES_H
+#define _TREES_H
+
+#include <trees/nodes.h>
+
+#include <analysis/x86_analysis.h>
+
+#include <stdint.h>
+
+/* trees and their routines */
+
+typedef void* (*node_mutator)(Node* node, void* value);
+typedef void* (*return_mutator)(
+    void* node_value, std::vector<void*> traverse_value, void* value);
+typedef Node* (*node_to_node)(void* head, void* node, void* peripheral_data);
+
+void* empty_ret_mutator(void* value, vector<void*> values, void* ori_value);
+
+class Tree {
+
+protected:
+  Node* head;
+
+  /* internal simplification routines */
+  /* fill as they are written */
+
+private:
+  void print_tree_recursive(Node* node, std::ostream& file);
+  void collect_all_nodes(Node* node, std::vector<Node*>& nodes);
+  static void copy_unrolled_tree_structure(
+      Node* head, Node* from, Node* to, void* peripheral_data,
+      node_to_node node_creation);
+  static bool are_trees_similar(std::vector<Node*> node);
+
+protected:
+  void* traverse_tree(
+      Node* node, void* value, node_mutator mutator,
+      return_mutator ret_mutator);
+  void copy_exact_tree_structure(
+      Tree* tree, void* peripheral_data, node_to_node node_creation);
+  void copy_unrolled_tree_structure(
+      Tree* tree, void* peripheral_data, node_to_node node_creation);
+
+public:
+  uint32_t num_nodes;
+  int32_t tree_num; /* this is for numbering the tree (most porabably based on
+                       output location - used in tree clustering) */
+  static uint32_t num_paras;
+  bool recursive;
+  bool dummy_tree;
+
+  Tree();
+  ~Tree();
+
+  Node* get_head();
+  void set_head(Node* node);
+
+  /* for tree simplification -> each type of tree will have its own
+   * serialization routines */
+  virtual std::string serialize_tree() = 0;
+  virtual void construct_tree(std::string stree) = 0;
+
+  /* tree manipulation routines */
+  void canonicalize_tree();
+  void simplify_tree();
+  void change_head_node();
+
+  /* for printing purposes */
+  void number_tree_nodes();
+  void print_tree(std::ostream& file);
+  void print_dot(std::ostream& file, std::string name, uint32_t number);
+
+  bool are_trees_similar(Tree* tree);
+  static bool are_trees_similar(std::vector<Tree*> trees);
+  bool is_recursive(Node* node, std::vector<mem_regions_t*>& regions);
+
+  void cleanup_visit();
+
+  /* some tree transformations */
+  void remove_assign_nodes();
+  std::vector<mem_regions_t*>
+  identify_intermediate_buffers(std::vector<mem_regions_t*> mem);
+  void remove_multiplication();
+  void simplify_immediates();
+  void remove_minus_nodes();
+  void remove_redundant_nodes();
+  void convert_sub_nodes();
+  void simplify_minus();
+  void verify_minus();
+  void remove_po_nodes();
+  void remove_or_minus_1();
+  void mark_recursive();
+  void remove_identities();
+};
+
+/* Conc_Tree - expression tree from the instruction trace */
+
+class Conc_Tree : public Tree {
+
+private:
+  /* data structure for keeping the hash table bins */
+  struct frontier_t {
+    Node** bucket;
+    int amount;
+  };
+
+  frontier_t* frontier; /*this is actually a hash table keeping pointers to the
+                           Nodes already allocated */
+  std::vector<uint32_t> mem_in_frontier; /* memoization structures for partial
+                                            mem and reg writes and reads */
+
+  bool func_inside;
+  uint32_t func_index;
+
+  Node* create_or_get_node(operand_t* opnd);
+  void remove_from_frontier(operand_t* opnd);
+
+  void get_full_overlap_nodes(std::vector<Node*>& nodes, operand_t* opnd);
+  void split_partial_overlaps(
+      std::vector<std::pair<Node*, std::vector<Node*>>>& nodes, operand_t* opnd,
+      uint32_t hash);
+  void get_partial_overlap_nodes(
+      std::vector<std::pair<Node*, std::vector<Node*>>>& nodes,
+      operand_t* opnd);
+
+  void number_parameters(Node* node, vector<mem_regions_t*> regions);
+
+public:
+  /* this governs which conditionals affect the tree -> needed for predicate
+   * tree generation */
+  struct conditional_t {
+
+    Jump_Info* jump_info;
+    uint32_t line_cond; // this is the cond_pc location
+    uint32_t line_jump;
+    Conc_Tree* tree; // we need to have two expressions for a single conditional
+    bool taken;
+  };
+
+  std::vector<conditional_t*> conditionals;
+
+  Conc_Tree();
+  ~Conc_Tree();
+
+  Node* search_node(operand_t* opnd);
+  int generate_hash(operand_t* opnd);
+  void add_to_frontier(int hash, Node* node);
+  void remove_registers_from_frontier();
+
+  // bool update_depandancy_backward(rinstr_t * instr, cinstr_t * cinstr,
+  // Static_Info * info, uint32_t line);
+  bool update_depandancy_backward(
+      rinstr_t* instr, cinstr_t* cinstr, Static_Info* info, uint32_t line,
+      vector<mem_regions_t*> region, vector<Func_Info_t*> func_info);
+  bool update_dependancy_forward(
+      rinstr_t* instr, uint32_t pc, std::string disasm, uint32_t line);
+  bool update_dependancy_forward_with_indirection(
+      rinstr_t* instr, uint32_t pc, std::string disasm, uint32_t line);
+  bool update_depandancy_forward_with_src(
+      rinstr_t* instr, uint32_t pc, std::string disasm, uint32_t line,
+      bool* src_dep);
+
+  void add_address_dependancy(Node* node, operand_t* opnds);
+  void process_forward_destination(operand_t* opnd);
+  void remove_dest_forward(operand_t* opnd);
+
+  void number_parameters(std::vector<mem_regions_t*> regions);
+  std::string serialize_tree();
+  void construct_tree(std::string stree);
+  void print_conditionals();
+  bool tree_add_to_frontier(rinstr_t* instr, Node* src);
+};
+
+/* Abs tree */
+class Abs_Tree : public Tree {
+
+public:
+  Abs_Tree();
+  ~Abs_Tree();
+
+  std::vector<std::pair<Abs_Tree*, bool>> conditional_trees;
+
+  void build_abs_tree_exact(
+      Conc_Tree* tree, std::vector<mem_regions_t*>& mem_regions);
+  void build_abs_tree_unrolled(
+      Conc_Tree* tree, std::vector<mem_regions_t*>& mem_regions);
+  void seperate_intermediate_buffers();
+
+  bool is_tree_recursive();
+  Abs_Node* find_indirect_node(Abs_Node* node);
+
+  std::vector<Abs_Node*> collect_input_nodes();
+
+  std::string serialize_tree();
+  void construct_tree(std::string stree);
+
+  void tag_parameters();
+  vector<Abs_Node*> retrieve_input_nodes();
+  vector<Abs_Node*> retrieve_parameters();
+
+  vector<Abs_Node*> get_buffer_region_nodes();
+
+  uint32_t get_maximum_dimensions();
+
+  void print_dot_algebraic(
+      std::ostream& file, std::string name, uint32_t number,
+      std::vector<std::string> vars);
+};
+
+/* compound abs tree */
+class Comp_Abs_Tree : public Tree {
+
+private:
+  static void build_compound_tree_unrolled(
+      Comp_Abs_Node* comp_node, std::vector<Abs_Node*> abs_nodes);
+  void traverse_trees(
+      std::vector<Abs_Node*> abs_nodes,
+      std::vector<std::pair<std::vector<Abs_Node*>, std::vector<int>>>&
+          node_pos);
+
+public:
+  Comp_Abs_Tree();
+  ~Comp_Abs_Tree();
+
+  void build_compound_tree_exact(std::vector<Abs_Tree*> abs_trees);
+  void build_compound_tree_unrolled(std::vector<Abs_Tree*> abs_trees);
+  void abstract_buffer_indexes();
+  Abs_Tree* compound_to_abs_tree();
+
+  std::string serialize_tree();
+  void construct_tree(std::string stree);
+};
 
 #endif
