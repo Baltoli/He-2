a71a884567364deda52027a5129d5785959724c6
Bruce Collie <brucecollie82@gmail.com>
2020-10-02 11:16:36 +0100

    Start to port scripts

---
+++ scripts/setup_folders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPTS_DIR=$(dirname $(realpath $0))
+ROOT_DIR=$(realpath "$SCRIPTS_DIR/..")
+
+cd $ROOT_DIR
+
+mkdir -p generated_files/output_files
+mkdir -p generated_files/filter_files
+mkdir -p generated_files/halide_files
+mkdir -p generated_files/log
