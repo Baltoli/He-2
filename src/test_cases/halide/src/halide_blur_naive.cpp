ca8393c43b270ffe01c5e8bdb78d3ea86c550494
Bruce Collie <brucecollie82@gmail.com>
2020-10-02 15:08:40 +0100

    Tests not quite working

--- src/test_cases/halide/src/halide_blur_naive.cpp
+++ src/test_cases/halide/src/halide_blur_naive.cpp
@@ -1,25 +1,26 @@
 #include <Halide.h>
 #include <vector>
 
-
 using namespace std;
 using namespace Halide;
 
+int main()
+{
+  ImageParam input(UInt(8), 2);
+  Func output("output"), clamped("clamped");
+  Var x("x"), y("y");
 
-int main(){
+  clamped(x, y) = cast<uint16_t>(
+      input(clamp(x, 1, input.width() - 1), clamp(y, 1, input.height() - 1)));
 
-	ImageParam input(UInt(8), 2);
-	Func output("output"), clamped("clamped");
-	Var x("x"), y("y");
+  output(x, y) = cast<uint8_t>(
+      (clamped(x - 1, y - 1) + clamped(x, y - 1) + clamped(x + 1, y - 1)
+       + clamped(x - 1, y) + clamped(x, y) + clamped(x + 1, y)
+       + clamped(x - 1, y + 1) + clamped(x, y + 1) + clamped(x + 1, y + 1))
+      / 9);
 
-	clamped(x, y) = cast<uint16_t>(input(clamp(x, 1, input.width() - 1), clamp(y, 1, input.height() - 1)));
+  output.compile_to_file("halide_blur_naive_gen", {input});
 
-	output(x, y) = cast<uint8_t>((clamped(x - 1, y - 1) + clamped(x, y - 1) + clamped(x + 1, y - 1)
-		+ clamped(x - 1, y) + clamped(x, y) + clamped(x + 1, y)
-		+ clamped(x - 1, y + 1) + clamped(x, y + 1) + clamped(x + 1, y + 1)) / 9);
-	
-	output.compile_to_file("halide_blur_naive_gen", input);
-	
-	return 0;
+  return 0;
 }
 
--- src/test_cases/halide/test/halide_blur_naive_test.cpp
+++ src/test_cases/halide/test/halide_blur_naive_test.cpp
@@ -1,36 +1,35 @@
-#include <windows.h>
+#include "halide_blur_naive_gen.h"
+
+#include <common/imageinfo.h>
+
+#include <Halide.h>
+
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string>
 
-#include "image_manipulation.h"
+Image<uint8_t> halide_function(Image<uint8_t> in)
+{
 
-extern "C" {
-#include "halide_blur_naive_gen.h"
+  Image<uint8_t> out(in.width(), in.height());
+
+  // Call it once to initialize the halide runtime stuff
+  halide_blur_naive_gen(in, out);
+
+  return out;
 }
 
-Image<uint8_t> halide_function(Image<uint8_t> in) {
+int main(int argc, char** argv)
+{
+  auto token = initialize_image_subsystem();
 
-    Image<uint8_t> out(in.width(), in.height());
+  string name(argv[1]);
+  Image<uint8_t> input = load_image<uint8_t>(argv[1]);
+  Image<uint8_t> output = halide_function(input);
+  save_image<uint8_t>(argv[2], output);
 
-    // Call it once to initialize the halide runtime stuff
-    halide_blur_naive_gen(in, out);
+  shutdown_image_subsystem(token);
 
-
-    return out;
-}
-
-int main(int argc, char **argv) {
-
-	ULONG_PTR token = initialize_image_subsystem();
-	
-	string name(argv[1]);
-	Image<uint8_t> input = load_image<uint8_t>(argv[1]);
-    Image<uint8_t> output = halide_function(input);
-	save_image<uint8_t>(argv[2], output);
-
-	shutdown_image_subsystem(token);
-
-    return 0;
+  return 0;
 }
