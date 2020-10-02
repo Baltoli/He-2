07c78e6f4839c128997a6d9364e23a667040cb2d
Bruce Collie <brucecollie82@gmail.com>
2020-10-02 16:25:52 +0100

    More compile

--- src/test_cases/halide/test/halide_blur_hvscan_test.cpp
+++ src/test_cases/halide/test/halide_blur_hvscan_test.cpp
@@ -1,38 +1,37 @@
-#include <windows.h>
+#include "halide_blur_hvscan_gen.h"
+
+#include <common/imageinfo.h>
+
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string>
 
-#include "image_manipulation.h"
+using namespace Halide;
 
-extern "C" {
-#include "halide_blur_hvscan_gen.h"
+Buffer<uint8_t> halide_function(Buffer<uint8_t> in, Buffer<uint8_t> out)
+{
+  // Call it once to initialize the halide runtime stuff
+  // halide_blur_hvscan_gen(in, out);
+  return out;
 }
 
-Image<uint8_t> halide_function(Image<uint8_t> in, Image<uint8_t> out) {
+int main(int argc, char** argv)
+{
+  auto token = initialize_image_subsystem();
+  char value = getchar();
 
-	// Call it once to initialize the halide runtime stuff
-	//halide_blur_hvscan_gen(in, out);
-	return out;
-}
+  std::string name(argv[1]);
+  Buffer<uint8_t> input = load_halide_image<uint8_t>(argv[1]);
+  Buffer<uint8_t> output(input.width() - 2, input.height() - 2);
 
-int main(int argc, char **argv) {
+  if (value == 'a') {
+    // halide_blur_hvscan_gen(input,input, input, 4, output);
+    // halide_blur_hvscan_gen(input,2.2, 5, output);
+    halide_blur_hvscan(*input.get(), *output.get());
+  }
+  save_halide_image<uint8_t>(argv[2], output);
+  shutdown_image_subsystem(token);
 
-	ULONG_PTR token = initialize_image_subsystem();
-	char value = getchar();
-
-	string name(argv[1]);
-	Image<uint8_t> input = load_image<uint8_t>(argv[1]);
-	Image<uint8_t> output(input.width() - 2, input.height() - 2);
-
-	if (value == 'a'){
-		//halide_blur_hvscan_gen(input,input, input, 4, output);
-		//halide_blur_hvscan_gen(input,2.2, 5, output);
-		halide_blur_hvscan_gen(input, output);
-	}
-	save_image<uint8_t>(argv[2], output);
-	shutdown_image_subsystem(token);
-
-	return 0;
+  return 0;
 }
