33ad833b2d6b45a19c5282eb13bb7b42d4feac19
Bruce Collie <brucecollie82@gmail.com>
2020-10-14 10:51:12 +0100

    More python porting

--- scripts/extract.py
+++ scripts/extract.py
@@ -14,21 +14,21 @@ def run_instrace(path, executable, args, debug, in_image):
                       'func',filter_folder + '\\filter_' + executable + '.log' , in_image, 'instrace')
         drclient.run_drclients(path, executable, args, debug, 'funcwrap,instrace',
                       'func',filter_folder + '\\filter_' + executable + '.log' , in_image, 'ins_distrace')
+
         
 def run_buildex(executable, in_image, out_image, debug, debug_level, dump, opts):
         parent_folder = os.environ.get('EXALGO_PARENT_FOLDER')
-        os.chdir(parent_folder + '/postprocess/buildex/build32/bin')
-        command = 'buildex.exe ';
-        args = [ '-exec ' + executable,
-                 '-in_image ' + in_image,
-                 '-out_image ' + out_image,
-                 '-debug ' + common.boolstr(debug),
-                 '-debug_level ' + debug_level,
-                 '-dump ' + dump,
-                  ]
-        for arg in args:
-                command += ' ' + arg
-        command += ' ' + opts
-        print(command)
-        p = subprocess.Popen(command)
-        p.communicate()
+        buildex_dir = os.path.join(parent_folder, 'build', 'postprocess', 'buildex', 'main')
+        buildex_exe = os.path.join(buildex_dir, 'buildex')
+
+        command = [
+            buildex_exe,
+            '-exec', executable,
+            '-in_image', in_image,
+            '-out_image', out_image,
+            '-debug', common.boolstr(debug),
+            '-debug_level', debug_level,
+            '-dump', dump
+        ]
+
+        subprocess.run(command)
